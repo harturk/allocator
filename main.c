fad81b11a34a8fbf7f466418897e8be2e647d1b1
Hartur <hartur.mainardi@edu.pucrs.br>
2023-11-19 14:56:25 -0300

    exclui trabalho podre

--- main.c
+++ main.c
@@ -3,7 +3,17 @@
 #include "mymemory.c"
 
 int main() {
-    mymemory_init(100);
+    // Como o pool de memória é estático, foi criado um método auxiliar para buscar
+    // o ponteiro para o pool de memória.
+    int size = 200;
+    mymemory_t *static_pool = mymemory_init(200);
+    allocation_t *block = mymemory_alloc(static_pool, 2 * sizeof(int));
+
+    printf("inicio do pool: %p\n", static_pool->pool);
+    printf("fim do pool: %p\n", size + static_pool->pool);
+
+    printf("inicio do pool: %p\n", block);
+    printf("fim do pool: %p\n", 2 * sizeof(int) + block);
 
     return 0;
 }
--- myMemory.c
+++ myMemory.c
@@ -1,35 +1,75 @@
-#include <stdlib.h>
 #include <stdio.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+// #include <sys/mman.h>
+#include <string.h>
 #include "mymemory.h"
 
-static mymemory_t* p;
+typedef struct {
+    void *start;
+    void *end;
+    void size;
+} occupied_space
 
-allocation_t* allocate_block(size_t size) {
-    allocation_t *block = (allocation_t *)&p[size + sizeof(allocation_t)];
-    block->size = sizeof(block);
-    block->start = &p[size + sizeof(allocation_t)];
+static mymemory_t *p;
+
+mymemory_t *getMymemory_t()
+{
+    return p;
+}
+
+
+allocation_t *allocate_block(mymemory_t *pool, size_t size)
+{
+    if (size > pool->total_size) {
+        printf("Tamanho do bloco maior que o tamanho total do pool\n");
+        return NULL;
+    }
+    allocation_t *block = malloc(sizeof(allocation_t));
+    if (block == NULL) {
+        printf("Erro ao alocar bloco\n");
+        return NULL;
+    }
+    // block = (allocation_t *)&pool[size + sizeof(mymemory_t)];
+    // block->start = &pool[size + sizeof(mymemory_t)];
+    block->size = size;
     block->next = NULL;
+    // Insere no head caso ainda não tenha sido alocado algum bloco
+    if (pool->head == NULL) {
+        pool->head = block;
+    }
+    printf("Block Criado no endereco %p\n", block);
     return block;
 }
 
-mymemory_t* mymemory_init(size_t size) {
-    if (p == NULL) {
-        p= malloc(sizeof(mymemory_t));
-        p->pool = malloc(8);
-        p->total_size = 8;
-        allocation_t block = allocate_block(size);
-        p->head = block;
-        printf("Pool Criado\n");
+mymemory_t *mymemory_init(size_t size)
+{
+    // Inicializa p, caso ainda nao tenha sido inicializado.
+    // A ideia é que este método seja rodado apenas um vez, para criar um pool único de memória.
+    if (p == NULL)
+    {
+        p = malloc(sizeof(mymemory_t));
+        p->pool = malloc(size);
+        p->total_size = size;
+        p->head = NULL;
+        printf("Pool Criado no endereco %p --- Tamanho %d\n", p->pool, sizeof(p->pool));
         return p;
     }
-
-    allocation_t* lastBlock = p->head;
-    while(p->head->next != NULL) {
-        lastBlock = p->head->next;
-    }
-    struct allocation block = allocate_block(size);
-    lastBlock->next = block;
 
     return p;
 }
 
+void* mymemory_alloc(mymemory_t *pool, size_t size)
+{
+    // Inicializa, caso não tenha sido inicializado ainda.
+    if (pool == NULL) {
+        printf("pool == NULL\n");
+        pool = mymemory_init(size);
+    }
+
+    // FIRST FIT
+    return allocate_block(pool, size);
+    return block->start;
+}
