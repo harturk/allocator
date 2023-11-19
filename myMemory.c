f9986559b793989baf233e13fe15e5089c499d0c
Hartur <hartur.mainardi@edu.pucrs.br>
2023-11-18 22:43:16 -0300

    Altera nomes + Insere métodos no header e documenta

--- myMemory.c
+++ myMemory.c
@@ -1,10 +1,10 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include "myMemory.h";
+#include "mymemory.h";
 #include "list.h";
 
-myMemory_t* myMemory_init(size_t size) {
-    myMemory_t *memory = malloc(sizeof(myMemory_t));
+mymemory_init* mymemory_init(size_t size) {
+    mymemory_t *memory = malloc(sizeof(mymemory_t));
     memory->pool = malloc(size);
     memory->total_size = size;
     memory->head = NULL;
--- myMemory.h
+++ myMemory.h
@@ -13,4 +13,27 @@ typedef struct {
     allocation_t *head;
 } mymemory_t
 
-myMemory_t* myMemory_init(size_t size);
+// Aloca o bloco de memória total e retorna um ponteiro para ele
+mymemory_t* mymemory_init(size_t size);
+
+// Tenta alocar um bloco de mem´oria de tamanho especificado. Se a alocação for bem-sucedida, 
+// retorna um ponteiro para o in´ıcio do bloco. Caso contrário, retorna NULL.
+void* mymemory alloc(mymemory t *memory, size t size);
+
+// Libera a alocação apontada por ptr. Se ptr não for uma alocação válida, a função
+// não deve fazer nada.
+void mymemory free(mymemory t *memory, void *ptr);
+
+// Exibe todas as alocações atuais, incluindo o início e o tamanho de cada alocação
+void mymemory display(mymemory t *memory);
+
+// Exibe estatísticas gerais sobre a memória, incluindo:
+//      – Número total de alocações
+//      – Memória total alocada (em bytes)
+//      – Memória total livre (em bytes)
+//      – O maior bloco contíguo de memória livre
+//      – Número de fragmentos de mem´oria livre (ou seja, blocos de memória entre alocações)
+void mymemory stats(mymemory t *memory);
+
+// Libera todos os recursos (incluindo todas as alocações e o bloco de memória total)
+void mymemory cleanup(mymemory t *memory);
